5df2d61212bb51a287724b8f64d10395f572b041
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-05-06 19:49:17 -0400

    Github.js
    I am giving this an update.

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import GitHubCalendar from "react-github-calendar";
+
 import { Row } from "react-bootstrap";
 import "../../style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -19,13 +19,7 @@ function Github() {
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
       </h1>
-     // <GitHubCalendar
-       // username="codeyab22"
-        //blockSize={15}
-        //blockMargin={5}
-        //theme={colourTheme}
-        //fontSize={16}
-      ///>
+     
     </Row>
   );
 }
