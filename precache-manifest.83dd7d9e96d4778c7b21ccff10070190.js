22089a4dc58b970026def0599dbd00c04f26f55e
Adia Boyd <codeyab703@gmail.com>
2021-05-07 02:55:20 -0400

    Updates

---
+++ precache-manifest.83dd7d9e96d4778c7b21ccff10070190.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f7839d2431e4fd965332c3de1230ea",
+    "url": "/Ready-React-Portfolio/index.html"
+  },
+  {
+    "revision": "94d1e513f53646fb9dc0",
+    "url": "/Ready-React-Portfolio/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "ca63ba1f84c593ea78f2",
+    "url": "/Ready-React-Portfolio/static/css/main.addaf861.chunk.css"
+  },
+  {
+    "revision": "94d1e513f53646fb9dc0",
+    "url": "/Ready-React-Portfolio/static/js/2.726b88e6.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/Ready-React-Portfolio/static/js/2.726b88e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca63ba1f84c593ea78f2",
+    "url": "/Ready-React-Portfolio/static/js/main.450b8e0a.chunk.js"
+  },
+  {
+    "revision": "77a3f2adb37dfe20385c",
+    "url": "/Ready-React-Portfolio/static/js/runtime-main.b3f4090e.js"
+  },
+  {
+    "revision": "af7da113e342bdff001c96288d5e5737",
+    "url": "/Ready-React-Portfolio/static/media/about.af7da113.png"
+  },
+  {
+    "revision": "85a8153083401ab22052328b0c71789f",
+    "url": "/Ready-React-Portfolio/static/media/adiaResume.85a81530.pdf"
+  },
+  {
+    "revision": "d4483ed441a15821714d231549857cc6",
+    "url": "/Ready-React-Portfolio/static/media/avatar.d4483ed4.svg"
+  },
+  {
+    "revision": "e4a392cac4b34b7fa4591a26048bb0e8",
+    "url": "/Ready-React-Portfolio/static/media/bad-to-dad.e4a392ca.jpg"
+  },
+  {
+    "revision": "ad2cbdc06eb23813c2d175756bc8185e",
+    "url": "/Ready-React-Portfolio/static/media/calendar-planner.ad2cbdc0.jpg"
+  },
+  {
+    "revision": "44cdc170c057c3948f3d2267c4dc98ff",
+    "url": "/Ready-React-Portfolio/static/media/fab-fitness.44cdc170.jpg"
+  },
+  {
+    "revision": "23b2eabece859679d248fcf6fde0426f",
+    "url": "/Ready-React-Portfolio/static/media/home-bg.23b2eabe.jpg"
+  },
+  {
+    "revision": "d8b8b7693e559b396a39e0c96c3066dd",
+    "url": "/Ready-React-Portfolio/static/media/home-main.d8b8b769.svg"
+  },
+  {
+    "revision": "36d1327fad78723174206e16b0afe3f0",
+    "url": "/Ready-React-Portfolio/static/media/logo.36d1327f.png"
+  },
+  {
+    "revision": "10ea33b7f97a19900aca4db8ee5e793b",
+    "url": "/Ready-React-Portfolio/static/media/password.10ea33b7.jpg"
+  },
+  {
+    "revision": "cf8c6935f68fde1543684d4a817f213a",
+    "url": "/Ready-React-Portfolio/static/media/pre.cf8c6935.svg"
+  },
+  {
+    "revision": "563aa8be8e517db0438132f293f73a0a",
+    "url": "/Ready-React-Portfolio/static/media/quarantine-and-chill.563aa8be.jpg"
+  },
+  {
+    "revision": "fe6d65af3fa0f87fb0e090a60634d29a",
+    "url": "/Ready-React-Portfolio/static/media/revitalize.fe6d65af.jpg"
+  },
+  {
+    "revision": "e4295aa822d1f86ea6cea68bc1a7e93c",
+    "url": "/Ready-React-Portfolio/static/media/weather-dashboard.e4295aa8.jpg"
+  }
+]);
