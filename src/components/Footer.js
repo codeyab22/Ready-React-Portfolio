7a3d3fd23e564327a60658b0f49a49ea676b6ed7
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-05-08 17:18:17 -0400

    Create Footer.js
    I am going to change these lines

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
+ 
 import React from "react";
-import "../style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   let date = new Date();
