92cb9c228bb010df87804be7f7ab2ec808e71de7
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-05-10 13:31:58 -0400

    Home.js
    I am going to fix the break in my code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import "../../style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeLogo from "../../Assets/home-main.svg";
-import Particle from "../Particle";
+// import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 
@@ -11,29 +11,7 @@ function Home() {
   return (
     <section>
       <Container fluid className="home-section" id="home">
-        <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There! <span className="wave">👋🏻</span>
-              </h1>
-
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> Adia Boyd</strong>
-              </h1>
-
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
-
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img src={homeLogo} alt="home pic" className="img-fluid" />
-            </Col>
-          </Row>
-        </Container>
+        
       </Container>
       <Home2 />
     </section>
